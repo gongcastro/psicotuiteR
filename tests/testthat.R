8953689b403a82afdc3ce31f0e08379afd885632
gongcastro <gonzalo.garciadecastro@upf.edu>
2021-11-11 18:57:36 +0100

    Merge testthat.R@

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,3 @@
-library(testthat)
-<<<<<<< HEAD
-library(psicotwitteR)
-
-test_check("psicotwitteR")
-=======
 library(psicotuiteR)
 
 test_check("psicotuiteR")
->>>>>>> e9658e7a254bdaa4f8303a110cceade05ca336c0
